08d9bc998a866a1a4a34489078aa9b84bf09b135
Perkinss27 <cyprien.gibault@efrei.net>
2024-04-27 19:08:51 +0200

    Added transportation_proposal class.

--- or_logics.py
+++ or_logics.py
@@ -5,28 +5,30 @@ class Problem():
     def __init__(self, data, pb_n) :
         
         #Stores the problem number (ex 1 for the file 1.txt).
-        self.__problem_number = pb_n
+        self.problem_number = pb_n
 
         #Will store the raw data from file.
         self.__data = data
 
         #Stores the prices for moving provision from provider_n to client_n.
-        self.__cost_matrix = []
+        self.cost_matrix = []
 
         #Stores the provisions for Pn
-        self.__provisions = []
+        self.provisions = []
 
         #Stores the orders for Cn
-        self.__orders = []
+        self.orders = []
 
         #Stores the number of providers.
-        self.__provider_n = 0
+        self.provider_n = 0
 
         #Stores the number of clients.
-        self.__client_n = 0
+        self.client_n = 0
 
         #Builds the required data.
         self.__build()
+
+        self.__transpo = TransportationProposal(self)
 
     #Function that will execute all the functions required to initialize the problem class.
     def __build(self) :
@@ -35,10 +37,10 @@ class Problem():
         lines = self.__data.split("\n")
 
         #Orders corresponds to the last line of the raw_data (txt file).
-        self.__orders = lines[-1].split(" ")
+        self.orders = lines[-1].split(" ")
         
         #The number of clients corresponds to the length of the orders table.
-        self.__client_n = len(self.__orders)
+        self.client_n = len(self.orders)
 
         #Iterating through the lines table without the last line (orders).
         for line in lines[:len(lines)-1] :
@@ -53,17 +55,17 @@ class Problem():
             provision = line_content[-1]
 
             #Appending extracted data to their respective tables.
-            self.__cost_matrix.append(prices)
-            self.__provisions.append(provision)
+            self.cost_matrix.append(prices)
+            self.provisions.append(provision)
 
         #The number of providers corresponds to the length of the provisions table.
-        self.__provider_n = len(self.__provisions)
+        self.provider_n = len(self.provisions)
 
-        """print("Clients :", self.__client_n)
-        print("Providers :", self.__provider_n)
-        print("Prices :", self.__cost_matrix)
-        print("Provisions :", self.__provisions)
-        print("Orders :", self.__orders)"""
+        """print("Clients :", self.client_n)
+        print("Providers :", self.provider_n)
+        print("Prices :", self.cost_matrix)
+        print("Provisions :", self.provisions)
+        print("Orders :", self.orders)"""
 
     #Prints the cost matrix (prices table).
     def print_cost_matrix(self) :
@@ -75,15 +77,15 @@ class Problem():
         table_content = []
 
         #Top left corner contains the problem's number.
-        table_header.append(self.__problem_number)
+        table_header.append(self.problem_number)
         
         #Adding each client to the header.
-        for client_n in range(self.__client_n) :
+        for client_n in range(self.client_n) :
             table_header.append(f"Client {client_n+1}")
 
         #Adding each provider and their prices to the cost matrix.
-        for provider_n in range(self.__provider_n) :
-            row = self.__cost_matrix[provider_n].copy()
+        for provider_n in range(self.provider_n) :
+            row = self.cost_matrix[provider_n].copy()
             row.insert(0, f"Provider {provider_n+1}")
             table_content.append(row)
 
@@ -92,7 +94,34 @@ class Problem():
 
     #Prints the Transportation proposal table
     def print_transportation_proposals(self) :
-        print("\Transportation Proposal matrix :\n")
+        
+        self.__transpo.print_transportation_proposal()
+        
+class TransportationProposal():
+
+    def __init__(self, pb: Problem) :
+        
+        #Related problem.
+        self.__problem = pb
+
+        #Will store the amount of suplly sent each providers to each clients.
+        self.__sent_amount = []
+
+        #Function executed to initialize data.
+        self.__build()
+    
+    #Initializing the TransportationProposal data.
+    def __build(self):
+        #When initialized all sent amounts are 0.
+        for provider_n in range(self.__problem.provider_n) :
+            self.__sent_amount.append([])
+            for client_n in range(self.__problem.client_n) :
+                self.__sent_amount[provider_n].append(0)
+    
+    #Prints the Transportation proposal table
+    def print_transportation_proposal(self) :
+
+        print("\nTransportation Proposal matrix :\n")
         #Will store the headers of the Transportation Proposal matrix.
         table_header = []
 
@@ -100,33 +129,38 @@ class Problem():
         table_content = []
 
         #Top left corner contains the problem's number.
-        table_header.append(self.__problem_number)
+        table_header.append(self.__problem.problem_number)
 
         #Adding each client to the header.
-        for client_n in range(self.__client_n) :
+        for client_n in range(self.__problem.client_n) :
             table_header.append(f"Client {client_n+1}")
 
         #Top right corners just displays "provisions".
         table_header.append("Provisions")
 
         #Adding each provider and their prices to the cost matrix.
-        for provider_n in range(self.__provider_n) :
+        for provider_n in range(self.__problem.provider_n) :
             row = []
             row.append(f"Provider {provider_n+1}")
 
             #Iterating through the cost matrix to fill each row.
-            for price in self.__cost_matrix[provider_n] :
-                row.append(f"{price}\n____\n0")
+            for client_n in range(len(self.__problem.cost_matrix[provider_n])) :
+                
+                #Getting the price from the cost matrix.
+                price = self.__problem.cost_matrix[provider_n][client_n]
+
+                #Getting the sent amount of supply from the sent_amount table.
+                associated_supply = self.__sent_amount[provider_n][client_n]
+
+                row.append(f"{price}\n____\n{associated_supply}")
             
             #Adding the provision for each provider
-            row.append(self.__provisions[provider_n])
+            row.append(self.__problem.provisions[provider_n])
 
             table_content.append(row)
 
-        last_row = []
-
         #Adding orders to last row.
-        last_row = self.__orders.copy()
+        last_row = self.__problem.orders.copy()
 
         #Bottom left corner just displays "Order".
         last_row.insert(0, "Order")
@@ -135,7 +169,7 @@ class Problem():
         total_orders = 0
 
         #computing the total of orders.
-        for order in self.__orders :
+        for order in self.__problem.orders :
             total_orders += int(order)
 
         #Adding the total of orders to the last row.
